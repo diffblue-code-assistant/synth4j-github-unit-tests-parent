926ff3b6e8dc4a9f6a18f788cd70c45a64dcae68
Ján Jendrušák <jan.jendrusak@diffblue.com>
2019-06-19 17:11:14 +0100

    Test project using '<parent>' in a POM file

---
+++ src/module/src/main/java/com/diffblue/synth4j/gitlab/Runner.java
@@ -0,0 +1,10 @@
+package com.diffblue.synth4j.gitlab;
+
+public final class Runner {
+  public static void main(final String[] args) {
+    // TODO Auto-generated method stub
+  }
+
+  private Runner() {
+  }
+}
